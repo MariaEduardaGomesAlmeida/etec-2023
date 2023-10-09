9545d7296cf71f1d052297ab0bab381aca3a3ffa
Maria Eduarda <143905191+MariaEduardaGomesAlmeida@users.noreply.github.com>
2023-10-09 11:54:50 -0300

    Add files via upload

---
+++ TPA/Idade.java
@@ -0,0 +1,10 @@
+
+public class Idade {
+  public static void main (String[]args) {
+	  int anoNasc,anoAtual,idade;
+	  anoNasc=2007;
+	  anoAtual=2023;
+	  idade=anoAtual-anoNasc;
+	  System.out.println("Apresente a idade: "+idade+" anos");
+  }
+}
---
+++ TPA/Triangulo.java
@@ -0,0 +1,10 @@
+
+public class Triangulo {
+   public static void main (String[]args) {
+	   double base,h,area;
+	   base=21;
+	   h=12;
+	   area=(base*h)/2;
+	System.out.println("Apresente a área do triângulo "+area);
+   }
+}
