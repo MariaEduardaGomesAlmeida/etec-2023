0ed494ffa780064f2db421fd74b18a1f1a2fabb5
Maria Eduarda <143905191+MariaEduardaGomesAlmeida@users.noreply.github.com>
2023-12-23 14:24:47 -0300

    Update ex001.java

--- TPA/Matriz/ex001.java
+++ TPA/Matriz/ex001.java
@@ -3,17 +3,14 @@ public class ex001 {
 	public static void main(String[] args) {
 		
 		
-		int matriz[][], i, j, numAnt=1;
+		int matriz[][], i, j, dobro=1;
 		matriz = new int [4][4];
-		
-		matriz[0][0] = 1;
-		
 		
 		//Matriz
 		for(i=0; i<4; i++) {
 			for(j=0; j<4; j++) {
-				matriz[i][j] = numAnt*2;
-				numAnt = matriz[i][j];
+				matriz[i][j] = dobro*2;
+				dobro++;
 						
 			}
 		}
